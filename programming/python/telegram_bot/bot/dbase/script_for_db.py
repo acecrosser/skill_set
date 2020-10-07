eef1e98ae6ac5127238646b535ab39e20bdcdd9f
acecrosser <acecrosser@yandex.ru>
2020-10-07 09:52:46 +0300

    Обновление Октября

--- programming/python/telegram_bot/bot/dbase/script_for_db.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-connect = psycopg2.connect(dbname='timl_money',
-                           user='timl',
-                           password='OSXC715MICHElle',
-                           host='localhost')
-
-cursor = connect.cursor()
